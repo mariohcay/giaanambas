4f403210dadbf781044016304a4b2d692efe68ac
mariohcay <mariohcay@gmail.com>
2023-04-13 12:18:17 +0700

    1

--- application/views/Auth/vLogin.php
+++ application/views/Auth/vLogin.php
@@ -14,10 +14,13 @@
                                     <h1 class="h4 text-gray-900 mb-0 font-weight-bold mb-4">Selamat Datang di<br>SISTEM PENGELOLAAN DATA JEMAAT GIA ANAMBAS MALANG</h1>
                                     <p class="mb-4">Silahkan Login terlebih dahulu</p>
                                 </div>
+                                <div class="d-flex justify-content-center mb-4">
+                                    <video id="video" width="320" height="240" autoplay></video>
+                                </div>
                                 <?= $this->session->flashdata('message'); ?>
                                 <form action="<?= base_url('Auth') ?>" method="POST" class="user">
                                     <div class="form-group">
-                                        <input type="text" class="form-control form-control-user" name="username" placeholder="Username" autocomplete="off" required="required">
+                                        <input type="text" class="form-control form-control-user" id="username" name="username" placeholder="Username" autocomplete="off" required="required">
                                     </div>
                                     <div class="form-group">
                                         <input type="password" class="form-control form-control-user" name="password" placeholder="Password" required="required">
@@ -33,3 +36,47 @@
         </div>
     </div>
 </div>
+
+<script>
+    // Access the user's camera and display a video stream
+    navigator.mediaDevices.getUserMedia({
+            video: true
+        })
+        .then(function(stream) {
+            var video = document.getElementById("video");
+            video.srcObject = stream;
+            video.onloadedmetadata = function(e) {
+                video.play();
+            };
+
+            // Set up QuaggaJS to scan for QR codes
+            Quagga.init({
+                inputStream: {
+                    constraints: {
+                        width: 640,
+                        height: 480,
+                    },
+                    target: document.querySelector('#video')
+                },
+                decoder: {
+                    readers: ['ean_reader']
+                }
+            }, function(err) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log('QuaggaJS ready to start scanning');
+                Quagga.start();
+            });
+
+            // When a QR code is detected, output the data to an input field
+            Quagga.onDetected(function(result) {
+                console.log('QR code detected:', result.codeResult.code);
+                document.getElementById('username').value = result.codeResult.code;
+            });
+        })
+        .catch(function(err) {
+            console.log('Error accessing camera:', err);
+        });
+</script>
